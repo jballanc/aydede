73428ab03720789ed29e2c0239f010e2621d8fb3
Josh Ballanco <jballanc@gmail.com>
2014-01-28 09:28:15 +0200

    Fix comment indentation

--- src/grammar.lua
+++ src/grammar.lua
@@ -1,15 +1,15 @@
 --[[ grammar.lua
 
- A simple LPeg grammar for scheme.
+  A simple LPeg grammar for scheme.
 
- In order to be as flexible as possible, this grammar only parses tokens from
- input. All interpretation and evaluation tasks are handled by the interpreter
- which is provided as an argument to the main function returned by this
- module.
+  In order to be as flexible as possible, this grammar only parses tokens from
+  input. All interpretation and evaluation tasks are handled by the interpreter
+  which is provided as an argument to the main function returned by this
+  module.
 
- Copyright (c) 2014, Joshua Ballanco.
+  Copyright (c) 2014, Joshua Ballanco.
 
- Licensed under the BSD 2-Clause License. See COPYING for full license details.
+  Licensed under the BSD 2-Clause License. See COPYING for full license details.
 
 --]]
 
