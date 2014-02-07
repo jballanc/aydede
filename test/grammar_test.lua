2a0980f23312d2379277d14aece1da1514ba9963
Josh Ballanco <jballanc@gmail.com>
2014-02-07 03:38:52 +0200

    Fix test mock to generate new table each time

--- test/grammar_test.lua
+++ test/grammar_test.lua
@@ -15,43 +15,45 @@ local P = require("lpeg").P
 local grammar = require("grammar")
 
 -- A mock parser that will fail for any function call by default
-local pmock = {}
 local function failed_call(table, key)
   return function (...)
     error("Unexpected call to method: "..tostring(key), 2)
   end
 end
-setmetatable(pmock, { __index = failed_call })
+
+local function mock(mock_funs)
+  return setmetatable(mock_funs, { __index = failed_call })
+end
 
 
 TestGrammar = {}
 
 function TestGrammar:test_string()
-  function pmock.string(str)
-    assert_is(str, "\"Hello, world\"")
-  end
+  p = mock({ string = function(str)
+                        assert_is(str, "\"Hello, world\"")
+                      end })
 
-  local g = grammar(pmock)
+  local g = grammar(p)
   g[1] = "String"
   P(g):match("\"Hello, world\"")
 end
 
 function TestGrammar:test_escaped_quote_in_string()
-  function pmock.string(str)
-    assert_is(str, "\"I say, \\\"this works.\\\"\"")
-  end
+  p = mock({ string = function(str)
+                        assert_is(str, "\"I say, \\\"this works.\\\"\"")
+                      end })
 
-  local g = grammar(pmock)
+  local g = grammar(p)
   g[1] = "String"
   P(g):match("\"I say, \\\"this works.\\\"\"")
 end
 
 function TestGrammar:test_symbol()
-  function pmock.symbol(str)
-    assert_is(str, "foo")
-  end
+  p = mock({ symbol = function(str)
+                        assert_is(str, "foo")
+                      end })
 
-  local g = grammar(pmock)
+  local g = grammar(p)
   g[1] = "Symbol"
 
   P(g):match("foo")
