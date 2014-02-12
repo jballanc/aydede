dc594b617fd5cb83879a0bff758772c9e6325c61
Josh Ballanco <jballanc@gmail.com>
2014-02-12 04:00:25 +0200

    Refactor parser assertions

--- test/grammar_test.lua
+++ test/grammar_test.lua
@@ -25,60 +25,37 @@ local function mock(mock_funs)
   return setmetatable(mock_funs, { __index = failed_call })
 end
 
+local function assert_parse(rule, str)
+  local rule_tbl = {}
+  rule_tbl[rule:lower()] = function(s)
+                             assert_is(s, str)
+                           end
+  p = mock(rule_tbl)
+  local g = grammar(p)
+  g[1] = rule
+  P(g):match(str)
+end
 
 TestGrammar = {}
 
 function TestGrammar:test_string()
-  p = mock({ string = function(str)
-                        assert_is(str, "\"Hello, world\"")
-                      end })
-
-  local g = grammar(p)
-  g[1] = "String"
-  P(g):match("\"Hello, world\"")
+  assert_parse("String", "\"Hello, world\"")
 end
 
 function TestGrammar:test_escaped_quote_in_string()
-  p = mock({ string = function(str)
-                        assert_is(str, "\"I say, \\\"this works.\\\"\"")
-                      end })
-
-  local g = grammar(p)
-  g[1] = "String"
-  P(g):match("\"I say, \\\"this works.\\\"\"")
+  assert_parse("String", "\"I say, \\\"this works.\\\"\"")
 end
 
 function TestGrammar:test_symbol()
-  p = mock({ symbol = function(str)
-                        assert_is(str, "foo")
-                      end })
-
-  local g = grammar(p)
-  g[1] = "Symbol"
-
-  P(g):match("foo")
+  assert_parse("Symbol", "foo")
 end
 
 function TestGrammar:test_decimal()
-  p = mock({ number = function(str)
-                        assert_is(str, "2.0")
-                      end })
-
-  local g = grammar(p)
-  g[1] = "Number"
-
-  P(g):match("2.0")
+  assert_parse("Number", "2.0")
 end
 
 function TestGrammar:test_negative()
-  p = mock({ number = function(str)
-                        assert_is(str, "-3.2")
-                      end })
-
-  local g = grammar(p)
-  g[1] = "Number"
-
-  P(g):match("-3.2")
+  assert_parse("Number", "-3.2")
 end
 
 LuaUnit:setOutputType("TAP")
